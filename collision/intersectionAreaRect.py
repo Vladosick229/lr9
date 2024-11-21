82f695d42a46970a11ada100f8b12e12daf86971
Vlad Sevruk <vladworking5@gmail.com>
2024-11-21 12:30:02 +0300

    Update intersectionAreaRect.py

--- collision/intersectionAreaRect.py
+++ collision/intersectionAreaRect.py
@@ -1,30 +1,30 @@
-def intersectionAreaRect():
-    first=[(-7.4, 0),(13.2, 12)]
-    second=[(-3.4, 1),(9.2, 10)]
-    x1=first[0][0]
-    y1=first[0][1]
-    x2=first[1][0]
-    y2=first[1][1]
-    x3=second[0][0]
-    y3=second[0][1]
-    x4=second[1][0]
-    y4=second[1][1]
-    #границы области пересечения
-    left=max(x1,x3)# левая
-    bottom = max(y1, y3) # нижняя
-    right = min(x2, x4) # правая
-    top = min(y2, y4) # верхняя
+class RectCorrectError():
 
-    width=right-left
-    height=top-bottom
-    if(width < -10 or height < -10):
-        raise ValueError('ширина или длина введены некоректно')
-    elif (width <= 0 or height <= 0):
-        print(0)
-    else:
-        print(width*height)
+ def intersectionAreaRect():
+     first=[(1, 1), (2, 2)]
+     second=[(3, 17), (13, 1)]
+     x1=first[0][0]
+     y1=first[0][1]
+     x2=first[1][0]
+     y2=first[1][1]
+     x3=second[0][0]
+     y3=second[0][1]
+     x4=second[1][0]
+     y4=second[1][1]
+     #границы области пересечения
+     left=max(x1,x3)# левая
+     bottom = max(y1, y3) # нижняя
+     right = min(x2, x4) # правая
+     top = min(y2, y4) # верхняя
+
+     width=right-left
+     height=top-bottom
+     if(width < -10 or height < -10):
+         raise RectCorrectError("ширина или длина хуевые")
+     elif (width <= 0 or height <= 0):
+         print(0)
+     else:
+         print(width*height)
 
 
-
-
-intersectionAreaRect()
+ intersectionAreaRect()
